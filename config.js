b6b9026e3e75e9eb87860f8ba43a7068f27bb3aa
MohamedArshad-7 <MohamedArshad-7@users.noreply.github.com>
2026-08-30 16:08:25 +0000

    deploy: aa0b1403dbd8bf24047b90082859e81369e2ddf9

---
+++ config.js
@@ -0,0 +1,16 @@
+// This file loads Firebase config from environment variables
+// DO NOT commit your actual API keys here!
+
+(function() {
+  // These will be replaced during deployment
+  const firebaseConfig = {
+  apiKey: "AIzaSyA27jsC1dhnKZlpn43I9XAnAPKhpskXLPI",
+  authDomain: "macro-tacker-95e57.firebaseapp.com",
+  projectId: "macro-tacker-95e57",
+  storageBucket: "macro-tacker-95e57.firebasestorage.app",
+  messagingSenderId: "675312773155",
+  appId: "1:675312773155:web:53c84e440263760be76996",
+  measurementId: "G-K0NRJ5FZM0"
+};
+  window.firebaseConfig = firebaseConfig;
+})();
